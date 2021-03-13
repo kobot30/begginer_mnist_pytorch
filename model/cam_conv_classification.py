71f0af956e0a99430d268d4909373327faefc95f
kobot <76274498+kobot30@users.noreply.github.com>
2021-03-13 22:20:15 +0900

    update

--- model/cam_conv_classification.py
+++ model/cam_conv_classification.py
@@ -2,30 +2,25 @@ import torch.nn as nn
 
 
 class CamConvClassification(nn.Module):
-    def __init__(self, num_feature=32):
+    def __init__(self):
         super(CamConvClassification, self).__init__()
-        self.num_feature = num_feature
 
         self.layer = nn.Sequential(
-            nn.Conv2d(1, self.num_feature, 3, 1, 1),
-            nn.BatchNorm2d(self.num_feature),
+            nn.Conv2d(1, out_channels=16, kernel_size=3, padding=1), # 28x28 pixcel
+            nn.BatchNorm2d(num_features=16),
             nn.ReLU(),
-            nn.Conv2d(self.num_feature, self.num_feature * 2, 3, 1, 1),
-            nn.BatchNorm2d(self.num_feature * 2),
+            nn.AvgPool2d(kernel_size=2, stride=2), # 14x14
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=32),
             nn.ReLU(),
-            nn.AvgPool2d(2, 2),
-
-            nn.Conv2d(self.num_feature * 2, self.num_feature * 4, 3, 1, 1),
-            nn.BatchNorm2d(self.num_feature * 4),
-            nn.ReLU(),
-            nn.AvgPool2d(2, 2),
-
-            nn.Conv2d(self.num_feature * 4, self.num_feature * 8, 3, 1, 1),
-            nn.BatchNorm2d(self.num_feature * 8),
+            nn.AvgPool2d(kernel_size=2, stride=2), # 7x7
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_features=64),
             nn.ReLU(),
         )
+
         self.fc_layer = nn.Sequential(
-            nn.Linear(self.num_feature * 8 * 7 * 7, 1000),
+            nn.Linear(64 * 7 * 7, 1000), # out_channels * pixcel * pixcel
             nn.ReLU(),
             nn.Linear(1000, 10)
         )
